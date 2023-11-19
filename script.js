a6bb2250068eb8dc9dd1bf74a8d57293cb5edaef
MrDrag0nXYT <147036304+MrDrag0nXYT@users.noreply.github.com>
2023-11-19 20:45:35 +0300

    updating website for mobile

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 if (window.innerWidth <= 480) {
     // alert("Внимание! Этот сайт может выглядеть неправильно на мобильных устройствах. Я в будущем это поправлю, если не забью на этот сайт. Спасибо за понимание");
 }
+
+mobileMenu.style.display = (mobileMenu.style.display === 'flex' || mobileMenu.style.display === '') ? 'none' : 'flex';
+
+function toggleMenu() {
+    var mobileMenu = document.getElementById('mobileMenu');
+    mobileMenu.style.display = (mobileMenu.style.display === 'flex' || mobileMenu.style.display === '') ? 'none' : 'flex';
+
+    var menuIcon = document.getElementById('mobileMenuButtonIMG');
+
+    if (menuIcon.src.endsWith('menu.svg')) {
+        menuIcon.src = 'images/close.svg';
+    } else {
+        menuIcon.src = 'images/menu.svg';
+    }
+}
